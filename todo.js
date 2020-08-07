2a6260dc318618ce56cfcf1b4a471baa058130b7
wxyzcctv <2846629226@qq.com>
2020-08-07 20:53:05 +0800

    完成代码的优化

--- todo.js
+++ todo.js
@@ -1,69 +1,78 @@
 var fs = require('fs');
+const path = require('path');
+
 const verb = process.argv[2]
 const content = process.argv[3]
 const content2 = process.argv[4]
+const dbPath = path.join(__dirname, 'db')
+
+enSureDb()
+
+const list = fetch()
+const n = content
 
 switch (verb) {
     case 'add':
-        if (fs.existsSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db')) {
-            const listContent = fs.readFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db').toString()
-            const list = JSON.parse(listContent)
-
-            list.push([content, false])
-            fs.writeFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db', JSON.stringify(list))
-            console.log(list)
-        } else {
-            fs.writeFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db', '')
-            const list = []
-
-            list.push([content, false])
-            fs.writeFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db', JSON.stringify(list))
-            console.log(list)
-        }
+        addTask(list, content)
         break;
     case 'list':
-        {
-            const listContent = fs.readFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db').toString()
-            const list = JSON.parse(listContent)
-            console.log(list)
-        }
-        break
+        break;
     case 'delete':
-        {
-            const n = content
-            const listContent = fs.readFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db').toString()
-            const list = JSON.parse(listContent)
-            list.splice(n - 1, 1)
-            fs.writeFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db', JSON.stringify(list))
-            console.log(list)
-        }
+        removeTask(list, n)
         break;
     case 'done':
-        {
-            const listContent = fs.readFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db').toString()
-            const list = JSON.parse(listContent)
-            const n = content
-
-            list[n - 1][1] = true
-            fs.writeFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db', JSON.stringify(list))
-            console.log(list)
-        }
+        markTaskDone(list, n)
         break;
     case 'edit':
-        {
-            const listContent = fs.readFileSync('D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db').toString()
-            const list = JSON.parse(listContent)
-            const n = content
-            list[n-1][0] = content2
-            fs.writeFileSync("D:\\工作学习\\Node.js\\Node.js入门\\node-demo-1\\db", JSON.stringify(list))
-            console.log(list)
-        }
-        break
+        editTask(list, n, content2)
+        break;
     default:
         console.log("你要进行的操作是: " + verb)
         console.log("我不知道你想干啥")
         break;
 }
 
+display(list)
+if (verb !== 'list') {
+    save(list)
+}
 
 
+// 帮助函数
+
+function enSureDb() {
+    if (!fs.existsSync(dbPath)) {
+        fs.writeFileSync(dbPath, '')
+    }
+}
+function fetch() {
+    const listContent = fs.readFileSync(dbPath).toString()
+    let list
+    try {
+        list = JSON.parse(listContent) || [] // 兜底，保证最少有一个[]
+    } catch (err) {
+        list = []
+    }
+    return list
+}
+function save(list) {
+    fs.writeFileSync(dbPath, JSON.stringify(list))
+}
+function display(list) {
+    for (let i = 0; i < list.length; i++) {
+        const markStatus = list[i][1] === true ? "[x]" : "[_]"
+        console.log(markStatus + ' 任务内容是: ' + list[i][0])
+    }
+}
+function addTask(list, content) {
+    list.push([content, false])
+}
+function removeTask(list, n) {
+    list.splice(n - 1, 1)
+}
+function markTaskDone(list, n) {
+    list[n - 1][1] = true
+}
+function editTask(list, n, newContent) {
+    list[n - 1][0] = newContent
+}
